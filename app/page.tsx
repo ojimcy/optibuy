95ee6057718f528b61022dd32a475371390099d3
ojimcy <ojimcy247@gmail.com>
2023-10-02 10:16:02 +0100

    home ux

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,45 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="px-6 md:px-20 py-20 border-2 border-blue-900">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Smart Shopping Starts Here:
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+            <h1 className="head-text">
+              Unleash the power of{' '}
+              <span className="text-blue-900">OptiBuy</span>
+            </h1>
+            <p className="mt-6">
+              Powerful, self-serve product growth analytics to help you convert,
+              engage and retain more.
+            </p>
+            SearchBar
+          </div>
+          HeroCarousel
+        </div>
+      </section>
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-16 gap-y-16">
+          {['Apple Iphon 15', 'Book', 'Sneakers'].map((product) => (
+            <div>{product}</div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
