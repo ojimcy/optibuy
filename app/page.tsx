004a7b20d13699c14f93a5c147c55f7877df64ea
ojimcy <ojimcy247@gmail.com>
2023-10-02 10:47:40 +0100

    search and hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import HeroCarousel from '@/components/HeroCarousel';
+import SearchBar from '@/components/SearchBar';
 import Image from 'next/image';
 import React from 'react';
 
 const Home = () => {
   return (
     <>
-      <section className="px-6 md:px-20 py-20 border-2 border-blue-900">
+      <section className="px-6 md:px-20 py-20">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
@@ -24,9 +26,9 @@ const Home = () => {
               Powerful, self-serve product growth analytics to help you convert,
               engage and retain more.
             </p>
-            SearchBar
+            <SearchBar />
           </div>
-          HeroCarousel
+          <HeroCarousel />
         </div>
       </section>
       <section className="trending-section">
